1749342edfe9159732c0bf79f66c80610cbebdf7
asbel.dev <aapaza@develoop.net>
2025-10-21 14:25:52 -0400

    quick: 14:25

---
+++ auto.sh
@@ -0,0 +1,7 @@
+# En tu ~/.bashrc o ~/.zshrc
+alias gp='git add . && git commit -m "quick: $(date +%H:%M)" && git push'
+alias gs='git status'
+alias gl='git log --oneline -5'
+
+
+source ~/.bashrc  # o source ~/.zshrc

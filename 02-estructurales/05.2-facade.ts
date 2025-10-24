929ccd6f660d4ac38d5df5e24a4c424773fb4d86
jasbel <asbel.elmec@gmail.com>
2025-10-23 21:04:13 -0400

    changes

--- 02-estructurales/05.2-facade.ts
+++ 02-estructurales/05.2-facade.ts
@@ -58,19 +58,28 @@ class Memory {
 // 2. Clase Facade - ComputerFacade
 
 class ComputerFacade {
+  cpu: CPU
+  memory: Memory;
+  hardDrive: HardDrive
   // TODO: Agregar los atributos necesarios CPU, Memory y HardDrive
 
   // TODO: Agregar el constructor para instanciar los atributos CPU, Memory y HardDrive
-  constructor() {}
+  constructor() {
+    this.cpu = new CPU()
+    this.memory = new Memory()
+    this.hardDrive = new HardDrive()
+  }
 
   startComputer(): void {
     console.log('\n%cIniciando la computadora...', COLORS.cyan);
 
     // TODO: ejecutar las operaciones necesarias para encender la computadora
     // 1. Cargar el sistema operativo en la memoria - memory.load(0, hardDrive.read(0, 1024))
+    this.memory.load(0, this.hardDrive.read(0, 1024))
     // 2. Saltar a la posición de memoria 0 - cpu.jump(0)
+    this.cpu.jump(0)
     // 3. Ejecutar las instrucciones del CPU - cpu.execute()
-
+    this.cpu.execute()
     console.log('Computadora lista para usar.\n');
   }
 
@@ -80,8 +89,11 @@ class ComputerFacade {
 
     // TODO: ejecutar las operaciones necesarias para apagar la computadora
     // 1. Detener las operaciones del CPU - cpu.stopOperations()
+    this.cpu.stopOperations()
     // 2. Liberar la memoria - memory.free()
+    this.memory.free()
     // 3. Cerrar el disco duro - hardDrive.close()
+    this.hardDrive.close()
 
     console.log('Computadora apagada.\n');
   }
